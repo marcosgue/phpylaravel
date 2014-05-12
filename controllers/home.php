ea856145ca4b331de162d863a2f7b775f3f8cc69
Marcos Guerrini <marcosguerrini@gmail.com>
2014-05-12 09:24:14 -0300

    Terminada clase 2.2
    Cambios en los archivos al terminar la clase 2.2 del curso de PHP y
    Laravel de Mejorando.la

--- controllers/home.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $confidencial = "wow such private very confidential";
-    $language = "PHP";
-    $titulo = "Mejorando.la";
-    
-    // Llamando una función
-    view('home', ['language' => $language, 'titulo' => $titulo]);
